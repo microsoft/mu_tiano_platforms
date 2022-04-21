d1d62e987222ebcb9b92cb3e7e3e360f509e5c53
brbarkel@microsoft.com <brbarkel@microsoft.com>
2022-04-21 14:36:36 -0700

    Add the demo app
    This app doesn't do much yet, but it proves that
    we can build the app, move it to the virtual drive,
    and launch the app from EFI shell.

---
+++ rust_demo/src/main.rs
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation
+//
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(not(test), no_std)]
+
+use r_efi::efi;
+
+#[export_name = "efi_main"]
+pub extern "C" fn app_entry(_h: efi::Handle, _st: *mut efi::SystemTable) -> efi::Status {
+    // NOTE: First, before anything, we need a panic handler (see below).
+    //       There are some ongoing efforts (e.g. https://github.com/Rust-for-Linux)
+    //       that are trying to address this requirement, but it's not complete yet.
+
+    efi::Status::SUCCESS
+}
+
+//
+// Panic Handler
+// Make sure this doesn't allocate, otherwise you'll play hell with your allocator.
+//
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
