3fbb9d450b8611436d6cf65914cf7235453d5867
Taylor Beebe <31827475+TaylorBeebe@users.noreply.github.com>
2023-07-21 09:10:33 -0700

    Update Test Exemptions (#643)

--- Platforms/QemuSbsaPkg/PlatformBuild.py
+++ Platforms/QemuSbsaPkg/PlatformBuild.py
@@ -29,9 +29,8 @@ from io import StringIO
 
 # Declare test whose failure will not return a non-zero exit code
 FAILURE_EXEMPT_TESTS = {
-    "VariablePolicyFuncTestApp.efi": datetime.datetime(2023, 4, 21, 0, 0, 0),
-    "DxePagingAuditTestApp.efi": datetime.datetime(2023, 4, 21, 0, 0, 0),
-    "BaseCryptLibUnitTestApp.efi": datetime.datetime(2023, 4, 21, 0, 0, 0),
+    "VariablePolicyFuncTestApp.efi": datetime.datetime(2023, 7, 20, 0, 0, 0),
+    "DxePagingAuditTestApp.efi": datetime.datetime(2023, 7, 20, 0, 0, 0),
 }
 
 # Allow failure exempt tests to be ignored for 90 days
