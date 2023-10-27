04a5cf855e3acd713264518901644bd0c76f26bf
Kun Qin <kuqin@microsoft.com>
2023-10-27 11:56:34 -0700

    Disable image protection

--- Platforms/QemuSbsaPkg/Library/SbsaQemuLib/SbsaQemuMem.c
+++ Platforms/QemuSbsaPkg/Library/SbsaQemuLib/SbsaQemuMem.c
@@ -68,7 +68,7 @@ SbsaQemuLibConstructor (
 
     MmSettings.HeapGuardPolicy.Fields.MmPageGuard                    = 1;
     MmSettings.HeapGuardPolicy.Fields.MmPoolGuard                    = 1;
-    DxeSettings.ImageProtectionPolicy.Fields.ProtectImageFromUnknown = 1;
+    DxeSettings.ImageProtectionPolicy.Fields.ProtectImageFromUnknown = 0;
     // THE /NXCOMPAT DLL flag cannot be set using non MinGW GCC
  #ifdef __GNUC__
     DxeSettings.ImageProtectionPolicy.Fields.BlockImagesWithoutNxFlag = 0;
