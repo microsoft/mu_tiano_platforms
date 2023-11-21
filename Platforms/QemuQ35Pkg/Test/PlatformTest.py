2e22dc5651769379eb8603ffb45e35cdeeb992a6
Joey Vagedes <joeyvagedes@microsoft.com>
2023-11-21 10:49:47 -0800

    update

--- Platforms/QemuQ35Pkg/Test/PlatformTest.py
+++ Platforms/QemuQ35Pkg/Test/PlatformTest.py
@@ -60,7 +60,7 @@ class TestManager(BuildSettingsManager, UefiBuilder):
         return PlatformBuild.CommonPlatform.PackagesPath
 
     def GetActiveScopes(self):
-        return PlatformBuild.CommonPlatform.Scopes
+        return ('qemu', 'qemuq35', 'edk2-build', 'cibuild', 'host-based-test')
     
     def GetName(self):
         return f"{PLATFORM_NAME}_HostBasedTest"
