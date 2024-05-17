faa2384781b4898c609ff03d2f0b129d940ab213
Kun Qin <kuqin@microsoft.com>
2024-05-17 13:30:16 -0700

    that will do?

--- Platforms/QemuSbsaPkg/PlatformBuild.py
+++ Platforms/QemuSbsaPkg/PlatformBuild.py
@@ -346,6 +346,53 @@ class PlatformBuilder(UefiBuilder, BuildSettingsManager):
 
         logging.info("Building TF-A")
 
+        # HACKHACK: This is to tell if this is running from a POSIX compatible shell
+        os_type = shell_environment.GetEnvironment().get_shell_var("OSTYPE")
+
+        if os_type is not None and "msys" in os_type:
+
+            import ctypes
+            import sys
+
+            xunicode = str if sys.version_info[0] > 2 else eval("unicode")
+
+            # If running under Msys2 Python, just use DLL name
+            # If running under non-Msys2 Windows Python, use full path to msys-2.0.dll
+            # Note Python and msys-2.0.dll must match bitness (i.e. 32-bit Python must
+            # use 32-bit msys-2.0.dll, 64-bit Python must use 64-bit msys-2.0.dll.)
+            msys = ctypes.cdll.LoadLibrary("msys-2.0.dll")
+            msys_create_path = msys.cygwin_create_path
+            msys_create_path.restype = ctypes.c_void_p
+            msys_create_path.argtypes = [ctypes.c_int32, ctypes.c_void_p]
+
+            # Initialise the msys DLL. This step should only be done if using
+            # non-msys Python. If you are using msys Python don't do this because
+            # it has already been done for you.
+            if sys.platform != "msys":
+                msys_dll_init = msys.msys_dll_init
+                msys_dll_init.restype = None
+                msys_dll_init.argtypes = []
+                msys_dll_init()
+
+            free = msys.free
+            free.restype = None
+            free.argtypes = [ctypes.c_void_p]
+
+            CCP_WIN_W_TO_POSIX = 3
+
+            def win2posix(path):
+                """Convert a Windows path to a msys path"""
+                result = msys_create_path(CCP_WIN_W_TO_POSIX, xunicode(path))
+                if result is None:
+                    raise Exception("msys_create_path failed")
+                value = ctypes.cast(result, ctypes.c_char_p).value
+                free(result)
+                return value
+
+            clang_path = win2posix(shell_environment.GetEnvironment().get_shell_var("CLANG_BIN"))
+        else:
+            clang_path = shell_environment.GetEnvironment().get_shell_var("CLANG_BIN")
+
         # Need to build fiptool separately because the build system will override LIB with LIBC for firmware builds
         cmd = "make"
         args = " DEBUG=1 V=1 fiptool MAKEFLAGS= LIB=\"" + shell_environment.GetEnvironment().get_shell_var("LIB") + "\""
@@ -355,7 +402,7 @@ class PlatformBuilder(UefiBuilder, BuildSettingsManager):
 
         # Then we can make the firmware images with the fiptool built above
         cmd = "make"
-        args = "CC=\"" + shell_environment.GetEnvironment().get_shell_var("CLANG_BIN").replace(os.sep, posixpath.sep) + "clang\""
+        args = "CC=\"" + clang_path + "clang\""
         args += " PLAT=" + self.env.GetValue("QEMU_PLATFORM").lower()
         args += " ARCH=" + self.env.GetValue("TARGET_ARCH").lower()
         args += " DEBUG=" + str(1 if self.env.GetValue("TARGET").lower() == 'debug' else 0)
