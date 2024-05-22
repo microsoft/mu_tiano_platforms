371e4678de3f29a524adc4f358bb425e609d1748
Kun Qin <kuqin@microsoft.com>
2024-05-22 16:27:25 -0700

    how about this junk?

--- Platforms/QemuSbsaPkg/PlatformBuild.py
+++ Platforms/QemuSbsaPkg/PlatformBuild.py
@@ -395,7 +395,7 @@ class PlatformBuilder(UefiBuilder, BuildSettingsManager):
 
         # Then we can make the firmware images with the fiptool built above
         cmd = "make"
-        args = "CC=clang"
+        args = "CC=\"" + ClangBin + "clang.exe\""
         args += " DEBUG=1 PLAT=" + self.env.GetValue("QEMU_PLATFORM").lower()
         args += " ARCH=" + self.env.GetValue("TARGET_ARCH").lower()
         args += " DEBUG=" + str(1 if self.env.GetValue("TARGET").lower() == 'debug' else 0)
