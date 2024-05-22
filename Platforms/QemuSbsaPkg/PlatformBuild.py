b979af4d9892aba946ee5952d72a74fd32386cfc
Kun Qin <kuqin@microsoft.com>
2024-05-22 15:18:55 -0700

    Even quicker?

--- Platforms/QemuSbsaPkg/PlatformBuild.py
+++ Platforms/QemuSbsaPkg/PlatformBuild.py
@@ -322,6 +322,93 @@ class PlatformBuilder(UefiBuilder, BuildSettingsManager):
         return 0
 
     def PlatformPreBuild(self):
+        interesting_keys = ["ExtensionSdkDir", "INCLUDE", "LIB", "LIBPATH", "UniversalCRTSdkDir",
+                            "UCRTVersion", "WindowsLibPath", "WindowsSdkBinPath", "WindowsSdkDir", "WindowsSdkVerBinPath",
+                            "WindowsSDKVersion", "VCToolsInstallDir", "Path"]
+        if self.env.GetValue("TOOL_CHAIN_TAG") == "CLANGPDB":
+            HostInfo = GetHostInfo()
+
+            # check to see if host is configured
+            # HostType for VS tools should be (defined in tools_def):
+            # x86   == 32bit Intel
+            # x64   == 64bit Intel
+            # arm   == 32bit Arm
+            # arm64 == 64bit Arm
+            #
+            HostType = shell_environment.GetEnvironment().get_shell_var("CLANG_VS_HOST")
+            if HostType is not None:
+                HostType = HostType.lower()
+                self.Logger.info(
+                    f"CLANG_VS_HOST defined by environment.  Value is {HostType}")
+            else:
+                #figure it out based on host info
+                if HostInfo.arch == "x86":
+                    if HostInfo.bit == "32":
+                        HostType = "x86"
+                    elif HostInfo.bit == "64":
+                        HostType = "x64"
+                else:
+                    # anything other than x86 or x64 is not supported
+                    raise NotImplementedError()
+
+            # CLANG_VS_HOST options are not exactly the same as QueryVcVariables. This translates.
+            VC_HOST_ARCH_TRANSLATOR = {
+                "x86": "x86", "x64": "AMD64", "arm": "not supported", "arm64": "not supported"}
+
+            # now get the environment variables for the platform
+            shell_env = shell_environment.GetEnvironment()
+            # Use the tools lib to determine the correct values for the vars that interest us.
+            vs_vars = QueryVcVariables(
+                interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType])
+            for (k, v) in vs_vars.items():
+                shell_env.set_shell_var(k, v)
+
+        HostInfo = GetHostInfo()
+        clang_exe = "clang.exe" if HostInfo.os == "Windows" else "clang"
+        ClangBin = shell_environment.GetEnvironment().get_shell_var("CLANG_BIN")
+        ClangBin_Default = "UNDEFINED"
+
+        if HostInfo.os == "Windows":
+            ClangBin_Default = "D:\\LLVM\\bin\\\\"  #need to escape the last slash as it seems to be removed
+            clang_exe += ".exe"
+        elif HostInfo.os == "Linux":
+            ClangBin_Default = "/LLVM/bin/"  #this isn't right
+        else:
+            pass
+        if ClangBin is not None:
+            self.Logger.info("CLANG_BIN is already set.")
+        else:
+            # see if clang is on path.
+            for path_entry in os.getenv("PATH").split(os.pathsep):
+                path_entry = os.path.normpath(path_entry)
+                if os.path.isfile(os.path.join(path_entry, clang_exe)):
+                    ClangBin = os.path.abspath(path_entry) + os.sep
+                    break
+            if ClangBin is None:
+                # Didn't find it on path - try the install default.
+                ClangBin = ClangBin_Default
+
+            shell_environment.GetEnvironment().set_shell_var("CLANG_BIN", ClangBin)
+
+        shell_environment.GetEnvironment().insert_path(ClangBin)
+        RunCmd("set", "")
+
+        # Then we can make the firmware images with the fiptool built above
+        cmd = "make"
+        args = "CC=clang"
+        args += " DEBUG=1 PLAT=" + self.env.GetValue("QEMU_PLATFORM").lower()
+        args += " ARCH=" + self.env.GetValue("TARGET_ARCH").lower()
+        args += " DEBUG=" + str(1 if self.env.GetValue("TARGET").lower() == 'debug' else 0)
+        args += " SPM_MM=1 EL3_EXCEPTION_HANDLING=1 ENABLE_SME_FOR_NS=0 ENABLE_SVE_FOR_NS=0"
+        args += " ENABLE_FEAT_HCX=1" # Features used by hypervisor
+        # args += " FEATURE_DETECTION=1" # Enforces support for features enabled.
+        # args += " BL32=" + os.path.join(op_fv, "BL32_AP_MM.fd")
+        args += " all fip"
+        # args += " -j $(nproc)"
+        ret = RunCmd(cmd, args, workingdir= self.env.GetValue("ARM_TFA_PATH"))
+        if ret != 0:
+            return ret
+
         return 0
 
     #
