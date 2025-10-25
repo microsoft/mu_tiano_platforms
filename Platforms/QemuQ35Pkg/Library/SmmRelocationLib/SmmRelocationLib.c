f39fc67ccd3d31baf264557164916a4dfc810aca
Kun Qin <kuqin@microsoft.com>
2025-10-24 23:00:53 -0700

    relocation x64

--- Platforms/QemuQ35Pkg/Library/SmmRelocationLib/SmmRelocationLib.c
+++ Platforms/QemuQ35Pkg/Library/SmmRelocationLib/SmmRelocationLib.c
@@ -422,7 +422,6 @@ SplitSmramHobForSmmRelocation (
   @retval Others                CPU SmmBase Relocation failed.
 
 **/
-// volatile BOOLEAN loop = TRUE;
 EFI_STATUS
 EFIAPI
 SmmRelocationInit (
@@ -440,12 +439,11 @@ SmmRelocationInit (
   SmmRelocationStart = 0;
   SmmStacks          = NULL;
 
-  DEBUG ((DEBUG_INFO, "SmmRelocationInit Start %p\n", SmmInitHandler));
+  DEBUG ((DEBUG_INFO, "SmmRelocationInit Start \n"));
   if (MpServices2 == NULL) {
     return EFI_INVALID_PARAMETER;
   }
 
-  // while (loop) {}
   InitializeSpinLock (&mSmmRelocationSemaphore);
 
   //
